8b1cca2b9695c6df35a57c659a48adb410f57153
Aldo Leka <aldo.leka@eurofiber.com>
2023-04-17 18:27:57 +0200

    Make possible to run all projects simultaneously.

---
+++ app/reactapp/src/setupProxy.js
@@ -0,0 +1,14 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const context = [
+    "/weatherforecast",
+];
+
+module.exports = function (app) {
+    const appProxy = createProxyMiddleware(context, {
+        target: 'https://localhost:7101',
+        secure: false
+    });
+
+    app.use(appProxy);
+};
